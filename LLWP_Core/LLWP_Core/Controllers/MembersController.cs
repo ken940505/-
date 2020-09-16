31fcd12a7135fce2c2f7a7b3d32d65ec17fa4b35
sam22556444 <sam22556444@gmail.com>
2020-09-16 16:36:08 +0800

    修改LOGIN

--- LLWP_Core/LLWP_Core/Controllers/MembersController.cs
+++ LLWP_Core/LLWP_Core/Controllers/MembersController.cs
@@ -50,7 +50,7 @@ namespace LLWP_Core.Controllers
             string fEmail1 = (p.txtAccount1);
             TMemberdata cust = _db.TMemberdata.FirstOrDefault(t => (t.FMeMail == fEmail && t.FMePass.Equals(p.txtPassword)) || (t.FMeMail == fEmail1 && t.FMePass.Equals(p.txtPassword1)));
 
-            if (cust == null || p.txtAccount != cust.FMeMail || p.txtPassword != cust.FMePass)
+            if (cust == null)
             {
                 HttpContext.Session.SetObject("SK_AUTHERROR", "帳密錯誤，登入失敗");
                 return RedirectToAction("Login");
